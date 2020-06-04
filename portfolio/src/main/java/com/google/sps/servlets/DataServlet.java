bce85b028e0e6bb33efffb05f9a9d3be29d6a10c
Kendrick <sharpekendrick@gmail.com>
2020-06-04 22:31:23 +0000

    fix timestamp sort

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -37,7 +37,7 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    Query query = new Query("Site").addSort("timestamp", SortDirection.ASCENDING);
+    Query query = new Query("Site").addSort("timestamp", SortDirection.descending);
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery results = datastore.prepare(query);
